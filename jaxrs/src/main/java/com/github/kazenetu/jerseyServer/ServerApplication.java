dc72485fb49ed930c6d34c06edcb52b8a2a7bab3
kazenetu <rakudafactory@gmail.com>
2017-02-24 22:05:20 +0900

    slf4j+logbackでログ出力の簡易版を作成

---
+++ jaxrs/src/main/java/com/github/kazenetu/jerseyServer/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.kazenetu.jerseyServer;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("app")
+public class ServerApplication extends ResourceConfig {
+
+    public ServerApplication() {
+        packages(this.getClass().getPackage().getName());
+    }
+}
